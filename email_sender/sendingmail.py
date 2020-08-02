9d016b81cf9203bab663868f7697199e302f6683
Alok Verma <vermaalok007@gmail.com>
2020-08-02 17:08:32 +0530

    emailSchedular pushed

---
+++ email_sender/sendingmail.py
@@ -0,0 +1,40 @@
+# the first step is always the same: import all necessary components:
+
+import pymysql
+
+from email_sender.MailSenderUtility import SendEmails
+
+
+def get_table_rows():
+    try:
+        db = pymysql.connect("localhost", "root", "alok@1990", "meetings")
+
+        # prepare a cursor object using cursor() method
+        cursor = db.cursor()
+
+        # execute SQL query using execute() method.
+        query = "SELECT * FROM calendar  WHERE start_time>=now()-INTERVAL 150 MINUTE  and send_notification=0 and meeting_type='meeting'"
+        cursor.execute(query)
+        result = cursor.fetchall()
+        body = dict()
+        for row in result:
+            body = {"title": row[2], "organizer": row[-1], "start_time": row[6], "meeting_id": row[1],
+                    "participants": row[9]}
+
+        participants_list = body.get("participants").split(",")
+        for userEmail in participants_list:
+            query = "SELECT FirstName FROM User WHERE FirstName IS NOT NULL UNION SELECT UserName AS FirstName FROM User WHERE FirstName IS NULL and Email=""\"{}\"".format(
+                userEmail)
+            cursor.execute(query)
+            result = cursor.fetchall()
+            if len(result) > 0:
+                userName = result[0]
+                body['userName'] = userName[0]
+                SendEmails().send_email(userEmail, body)
+            else:
+                SendEmails().send_email(userEmail, body)
+    except Exception as e:
+        print("Error in getting data and sending email==> ", e)
+
+
+get_table_rows()
