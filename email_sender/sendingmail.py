5c3ec98f758695e43a051a6a54ca6f23e16ce331
Alok Verma <vermaalok007@gmail.com>
2020-08-04 21:11:49 +0530

    meaasge post api changed

--- email_sender/sendingmail.py
+++ email_sender/sendingmail.py
@@ -1,14 +1,19 @@
 # the first step is always the same: import all necessary components:
 
+import json
 import smtplib
+import time
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from matterhook import Webhook
 
 import pymysql
+import requests
 
 
 class SendEmails(object):
+    def __init__(self):
+        self.db = pymysql.connect("localhost", "root", "alok@1990", "meetings")
+
     def send_email(self, recipients, body):
         try:
             email_list = []
@@ -24,7 +29,7 @@ class SendEmails(object):
             smtp_ssl_host = 'mail.nattymail.com'  # smtp.mail.yahoo.com
             smtp_ssl_port = 587
             username = 'noreply@nattymeet.com'
-            password = 'U3g5ES9UjG@2020'
+            password = '3A22HTBgJV'
             sender = 'noreply@nattymeet.com'
 
             msg = MIMEMultipart('alternative')
@@ -48,70 +53,82 @@ class SendEmails(object):
         except Exception as e:
             raise Exception("Sorry we could'nt send email for meeting_id {}".format(meeting_id))
 
+    def call_matter_hook(self, body, channel_id):
+        try:
+            url = "https://rnd.nattycb.com/api/v4/posts"
+            meeting_id = body['meeting_id']
 
-def call_matter_hook(channel_name):
-    try:
-        #getdata from channel table
-        mwh = Webhook('https://rnd.nattycb.com', 'x5m5u4ej6fb1mf151pqysw4fic')
-        mwh.send(message='meeting reminder', channel=channel_name, username='schedule_meeting',icon_url='https://rnd.nattycb.com/static/images/favicon/favicon-32x32.png')
-    except Exception as e:
-        print("exception occurred while calling webhook",e)
+            payload = {"channel_id": channel_id, "message": "",
+                       "props": {"type": "meeting_reminder", "meeting_id": meeting_id,
+                                 "meeting_name": "meeting_reminder", "startdate": body['start_time']}}
 
-def get_table_rows():
-    try:
-        db = pymysql.connect("localhost", "mattermost", "RFzWV5kdJl", "mattermost")
+            headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer 4bqmzeh9cirn7bby4jtpgatwjc'
+            }
 
-        # prepare a cursor object using cursor() method
-        cursor = db.cursor()
+            response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
 
-        # execute SQL query using execute() method.
-        query = "SELECT * FROM calendar  WHERE start_time>=now()-INTERVAL 15 MINUTE  and send_notification=0 and meeting_type='meeting'"
-        cursor.execute(query)
-        result = cursor.fetchall()
-        if len(result) > 0:
-            body = dict()
-            for row in result:
-                body = {"title": row[2], "organizer": row[-1], "start_time": row[6], "meeting_id": row[1],
-                        "participants": row[9]}
+            print(response.text)
 
-            participants_list = body.get("participants").split(",")
-            for userEmail in participants_list:
-                query = "SELECT FirstName FROM User WHERE FirstName IS NOT NULL UNION SELECT UserName AS FirstName FROM User WHERE FirstName IS NULL and Email=""\"{}\"".format(
-                    userEmail)
-                cursor.execute(query)
-                result = cursor.fetchall()
-                if len(result) > 0:
-                    userName = result[0]
-                    body['userName'] = userName[0]
-                    if row[5] is not None:
-                        channel_name_list = row[5].split(",")
-                        for channel_id in channel_name_list:
-                            try:
-                                query_channel_name = "SELECT Name FROM meetings.channels where Id = ""\"{}\"".format(channel_id.strip())
-                                cursor.execute(query_channel_name)
-                                res =cursor.fetchone()
-                                if res is not None:
-                                    call_matter_hook(res[0])
-                            except Exception as e:
-                                print("unable to call matter hook")
-                    try:
-                        SendEmails().send_email(userEmail, body)
-                    except Exception as e:
-                        print("error sending email")
-                else:
-                    try:
-                        SendEmails().send_email(userEmail, body)
-                    except Exception as e:
-                        print("error sending email without user name")
-        else:
-            print("No meeting scheduled")
-    except Exception as e:
-        print("Error in getting data and sending email==> ", e)
+        except Exception as e:
+            print("exception occurred while calling webhook", e)
+
+    def get_table_rows(self):
+        try:
+            # prepare a cursor object using cursor() method
+            cursor = self.db.cursor()
+
+            # execute SQL query using execute() method.
+            query = "SELECT * FROM calendar  WHERE start_time>=now()-INTERVAL 16 MINUTE  and send_notification=0 and meeting_type='meeting'"
+            cursor.execute(query)
+            result = cursor.fetchall()
+            if len(result) > 0:
+                body = dict()
+                for row in result:
+                    body = {"title": row[2], "organizer": row[-1], "start_time": row[6], "meeting_id": row[1],
+                            "participants": row[9]}
+
+                participants_list = body.get("participants").split(",")
+                for userEmail in participants_list:
+                    query = "SELECT FirstName FROM User WHERE FirstName IS NOT NULL UNION SELECT UserName AS FirstName FROM User WHERE FirstName IS NULL and Email=""\"{}\"".format(
+                        userEmail)
+                    cursor.execute(query)
+                    result = cursor.fetchall()
+                    if len(result) > 0:
+                        userName = result[0]
+                        body['userName'] = userName[0]
+                        if row[5] is not None:
+                            channel_name_list = row[5].split(",")
+                            for channel_id in channel_name_list:
+                                try:
+                                    self.call_matter_hook(body, channel_id)
+                                except Exception as e:
+                                    print("unable to call matter hook")
+                        try:
+                            query = "update calendar set send_notification = 1 where meeting_id=""\"{}\"".format(
+                                body["meeting_id"])
+                            SendEmails().send_email(userEmail, body)
+                            cursor = self.db.cursor()
+                            cursor.execute(query)
+                        except Exception as e:
+                            print("error sending email ", e)
+                    else:
+                        try:
+                            SendEmails().send_email(userEmail, body)
+                        except Exception as e:
+                            print("error sending email without user name")
+            else:
+                print("No meeting scheduled")
+        except Exception as e:
+            print("Error in getting data and sending email==> ", e)
 
 
 while True:
     try:
         print("schedular started")
-        get_table_rows()
+        SendEmails().get_table_rows()
+        time.sleep(60)
+
     except Exception as e:
         print("Exception in schedular")
